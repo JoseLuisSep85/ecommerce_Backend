461151ae7b62899b4aa6d1e9cf585396a266fd4b
“JoseLuisSep85“ <jlsv@outlook.cl>
2023-10-05 21:08:05 -0300

    creando practica integradora

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const viewsRouter = require('./routes/views.router')
+const app = express();
+const PORT = 8080;
+
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('views engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+
+app.use('/', viewsRouter)
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor conectado al Puerto ${PORT}`)  
+})
